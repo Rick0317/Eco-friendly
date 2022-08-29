d8c24dea30010580dea7a165baa8c73d9d012337
Rick0317 <rick.hey.huang@gmail.com>
2022-08-29 09:58:31 +0900

    RickCommit

---
+++ Rick_Code/image_extraction.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.bbc.com/'
+
+html = requests.get(url)
+soup = BeautifulSoup(html.content, "html.parser")
+print(soup.find_all(""))
