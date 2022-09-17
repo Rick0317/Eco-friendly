b35e1b039a1c4a6ede0d26ac779ef889575b5b02
Rick0317 <rick.hey.huang@gmail.com>
2022-09-17 15:05:50 -0400

    afterPulling

--- images.js
+++ images.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  setTimeout(
     chrome.tabs.query(
         {active: true}, // queryInfo
@@ -28,39 +27,3 @@
         }
     ), 1000
  )
-=======
-let protocolVersion = "1.2";
-
-chrome.tabs.query(
-  {active: true},
-
-  function(tabs){
-    // Fetching html content of the website
-    fetch(`${tabs[0].url}`)
-    .then(function (response) {
-        switch (response.status) {
-            // status "OK"
-            case 200:
-                return response.text();
-            // status "Not Found"
-            case 404:
-                throw response;
-        }
-    })
-    .then(function (template) {
-        html = template //string
-        console.log((html.match(/<img>|img/g)).length)
-        if((html.match(/<img>|img/g)).length < 50) {
-          document.getElementById("image_count").innerHTML = `&#128516`
-        }else {
-          document.getElementById("image_count").innerHTML = `&#128529`
-        }
-
-    })
-    .catch(function (response) {
-        // "Not Found"
-        response.statusText
-    });
-  }
-);
->>>>>>> 8964b8a67ae01c408fa6c01db1d7a877de925bda
