f8a52b093191c2693cf3556740842e29a07f91df
takedakento <107985058+takedakento@users.noreply.github.com>
2022-09-24 23:35:25 -0400

    Edit network

--- network.js
+++ network.js
@@ -1,25 +1,28 @@
 const protocolVersion = "1.2";
 let kb = 0;
-getNetwork();
+getDebugger();
 
-async function getNetwork() {
+async function getDebugger() {
   const tabs = await chrome.tabs.query({ active: true });
   const debuggee = {tabId: tabs[0].id};
   chrome.debugger.attach(debuggee, protocolVersion)
     .then(() => {
-      if (chrome.runtime.lastError) {
-        return;
-      }
-      chrome.debugger.sendCommand(debuggee, "Network.enable")
-        .then(() => {
-          chrome.debugger.onEvent.addListener((source, method, params) => {
-            if (method == "Network.loadingFinished") {
-              kb += params.encodedDataLength / 1000;
-              document.getElementById("data_received").innerHTML = `${kb} KB`;
-            }
-          })
-        })
-        .catch()
+      getNetwork(debuggee);
     })
-    .catch()
+    .catch(() => {
+      getNetwork(debuggee);
+    })
+}
+
+function getNetwork(debuggee) {
+  chrome.debugger.sendCommand(debuggee, "Network.enable")
+  .then(() => {
+    chrome.debugger.onEvent.addListener((source, method, params) => {
+      if (method == "Network.loadingFinished") {
+        kb += params.encodedDataLength / 1000;
+        document.getElementById("data_received").innerHTML = `${kb} KB`;
+      }
+    })
+  })
+  .catch()
 }
