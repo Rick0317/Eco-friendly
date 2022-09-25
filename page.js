6c1bc9b1ae177f6cccc19e2d6a0bda5f4db18ff6
Rick0317 <rick.hey.huang@gmail.com>
2022-09-25 03:12:34 -0400

    modifiedFrontend

--- page.js
+++ page.js
@@ -1,9 +1,8 @@
-getPage()
-
 async function getPage() {
   const tabs = await chrome.tabs.query({ active: true });
   const tabId = tabs[0].id;
-  chrome.pageCapture.saveAsMHTML({ tabId: tabId }, (mhtmlData) => {
-    console.log(mhtmlData.size);
+  chrome.pageCapture.saveAsMHTML({ tabId: tabId }, (htmlData) => {
+    document.getElementById("page_data").innerHTML=`${Number((htmlData.size/1000000).toFixed(2))}MB`
   })
 }
+getPage()
